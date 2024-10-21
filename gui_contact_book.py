8aad76a07cac32e5e1b4804529432385acf524ff
vpadua <vgcpad@gmail.com>
2024-10-21 17:07:28 -0400

    updated ContactBook class, unfinished

--- gui_contact_book.py
+++ gui_contact_book.py
@@ -7,23 +7,21 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
 
-class App(tk.Tk):
-	def __init__(self, title, size):
-		
-		# main setup
-		super().__init__()
-		self.title(title)
-		self.geometry(f'{size[0]}x{size[1]}')
-		self.minsize(size[0],size[1])
+class ContactBook(tk.Tk):
+	def __init__(self):
+		self.contacts = []
 
-		# widgets
-        # this is where you call your other classes
-		
-		# run 
+
+
+	def add_contact(self, contact):
+		self.contacts.append(contact)
+
+	def search_contact(self, name):
+
+
 		self.mainloop()
 
 # Write your own classes below
-
 
 
 
@@ -35,4 +33,4 @@ contactBook = {'John Jo': '555-555-5555', 'Mugi Mu': '556-556-5656', 'Mike Mi' :
 
 
 # execution code
-App('Class based app', (600,600)) #edit to insert your own title and window size
+ContactBook('Contact Book', (600,600)) #edit to insert your own title and window size
