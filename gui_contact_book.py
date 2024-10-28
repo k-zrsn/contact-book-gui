bbc130bc3efcd621d35ad2398e7d9d0310450a2a
Virgil Padua <vgcpad@gmail.com>
2024-10-28 18:41:45 -0400

    created contact_information class, started to add gui elements

--- gui_contact_book.py
+++ gui_contact_book.py
@@ -1,11 +1,10 @@
 ### Contact book (with GUI)
 ### A program that allows users to add, search, and delete contacts.
 
-
-
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+
 
 
 ### Application class
@@ -22,19 +21,30 @@ class app(tk.Tk):
         # this is where you call your other classes
 		###self.menu = Menu(self) # delete this example in your own code
 
-
 		# run 
 		self.mainloop()
 
 
+
+### Contact class
+class contact_information:
+	def __init__(self, name, phone):
+		self.name = name
+		self.phone = phone
+
+	def __str__(self):
+		return f"{self.name}: {self.phone}"
+
+
+
 ### Contact book class
-class contactBook:
+class contact_book:
 	def __init__(self):
 		self.contacts = []
 
 
 
-	def add_contact(self, contact):
+	def add_contact_to_book(self, contact):
 		self.contacts.append(contact)
 
 	def search_contact(self, name):
@@ -45,6 +55,15 @@ class contactBook:
 
 
 
+def add_contact():
+    name = name_entry.get()
+    phone = phone_entry.get()
+    if name and phone:
+        contact_book.add_contact_to_book(contact_information(name, phone))
+        name_entry.delete(0, tk.END)
+        phone_entry.delete(0, tk.END)
+    else:
+        messagebox.showwarning("Input Error", "Please enter both name and phone number.")
 
 
 
@@ -57,4 +76,23 @@ contactList = {'John Jo': '555-555-5555', 'Mugi Mu': '556-556-5656', 'Mike Mi' :
 
 
 ### run
-app('Contact Book', (600,600))
+app = tk.Tk()
+app.title = ("Contact Book")
+contact_book = contact_book()
+
+
+name_entry = tk.Entry(app, width=30)
+name_entry.pack(pady=10)
+name_entry.insert(0, "Name")
+
+phone_entry = tk.Entry(app, width=30)
+phone_entry.pack(pady=10)
+phone_entry.insert(0, "Phone Number")
+
+button_frame = tk.Frame(app)
+button_frame.pack(pady=10)
+
+add_button = tk.Button(button_frame, text="Add Contact", command=add_contact)
+add_button.pack(side=tk.LEFT, padx=5)
+
+app.mainloop()
